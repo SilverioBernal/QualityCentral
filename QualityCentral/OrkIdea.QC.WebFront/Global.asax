fec4e76fe9bc6ceede3fa692027c58fd16767a35
silverio.bernal@orkidea.co <silverio.bernal@orkidea.co>
2015-04-02 09:25:33 -0500

    Proyecto vacio

---
+++ QualityCentral/OrkIdea.QC.WebFront/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OrkIdea.QC.WebFront.MvcApplication" Language="C#" %>
---
+++ QualityCentral/OrkIdea.QC.WebFront/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OrkIdea.QC.WebFront.Startup))]
+namespace OrkIdea.QC.WebFront
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
